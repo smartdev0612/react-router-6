e73c8d0f1a97f64a79ba9f6d92f570c3b256879e
daniel0612 <danieldev0612@gmail.com>
2022-04-25 04:06:37 -0700

    protected route, url params

--- src/pages/SharedLayout.js
+++ src/pages/SharedLayout.js
@@ -1,11 +1,13 @@
 import { Link, Outlet } from 'react-router-dom'
 import Navbar from '../components/Navbar'
+import StyledNavbar from '../components/StyledNavbar'
 
 const Home = () => {
   return (
-    <section className='section'>
-      <h2>Home Page</h2>
-    </section>
+    <>
+      <StyledNavbar />
+      <Outlet />
+    </>
   );
 };
 export default Home;
